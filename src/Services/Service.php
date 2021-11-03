81623f6e7b2fc8c7995e35e5fb235353396f0b12
Andreea <andreea@code23.com>
2021-11-03 10:03:32 +0000

    switch x-mpe-origin to env variable

--- src/Services/Service.php
+++ src/Services/Service.php
@@ -19,8 +19,10 @@ class Service
     {
         // update headers with origin
         $this->headers = array_merge($this->headers, [
-            'X-MPE-Origin' => request()->header('host') ?? config('marketplace-laravel-sdk.app.url'),
+            'X-MPE-Origin' => config('marketplace-laravel-sdk.http.origin'),
         ]);
+        // TODO - put below back in after the domain matched the request origin
+        //'X-MPE-Origin' => request()->header('host') ?? config('marketplace-laravel-sdk.app.url'),
 
         // new http client with options expecting json from the api
         $this->client = Http::withHeaders($this->headers)->withOptions($this->getOptions())->acceptJson();
