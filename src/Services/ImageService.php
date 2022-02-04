2b789eee8da1e564199ccec2f19e5ae51b384d0b
Isaac Fennell <isaac@code23.com>
2022-02-04 17:50:03 +0000

    image filters

--- src/Services/ImageService.php
+++ src/Services/ImageService.php
@@ -4,6 +4,30 @@ namespace Code23\MarketplaceLaravelSDK\Services;
 
 class ImageService extends Service
 {
+
+    /**
+     * Return just the image_paths array of the featured image in an images object
+     *
+     * @param $images the images object to be filtered
+     */
+    public function imagesFeaturedFilter($images)
+    {
+        // return different image sizes paths if found, else null
+        return collect($images)->where('featured', true)->first()['image_paths'] ?? null;
+    }
+
+    /**
+     * Return just the image_paths array of an images object, for a given image category
+     *
+     * @param $images the images object to be filtered
+     * @param String $category - e.g. 'logo'
+     */
+    public function imagesPathsFilter($images, String $category)
+    {
+        // return different image sizes paths if found, else null
+        return collect($images)->where('category', $category)->first()['image_paths'] ?? null;
+    }
+
     /**
      * Prepare an image object for uploading to MPE.
      *
