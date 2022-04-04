8e22af70a969252bd76dddcf4a584ff234b9a619
Timos Zabetakis <timoszab@gmail.com>
2022-04-04 10:05:05 +0000

    adds facade and service for address lookup

---
+++ src/Services/AddressLookupService.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Code23\MarketplaceLaravelSDK\Services;
+
+use Exception;
+use Illuminate\Http\Request;
+
+class AddressLookupService extends Service
+{
+    /**
+     * Retrieve address by postcode
+     *
+     * @param String $postcode
+     * 
+     */
+    public function findByPostcode(String $postcode)
+    {
+        // send request
+        $response = $this->http()->get($this->getPath() . '/postcoder/lookup', [
+            'postcode' => $postcode
+        ]);
+
+        // api call failed
+        if ($response->failed()) throw new Exception('A problem was encountered whilst attempting to retrieve the address.', 422);
+
+        // any other errors
+        if ($response['error']) throw new Exception($response['message'], $response['code']);
+
+        // if successful, return address as collection
+        return $response->json()['data'] ? collect($response->json()['data']) : collect();
+
+        // return json payload
+        if ($response->successful()) return $response->json();
+    }
+    
+}
