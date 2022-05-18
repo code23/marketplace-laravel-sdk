e59e05b24688a154a756a5f1c11eaeb4ef6e1df1
Isaac Fennell <isaac@code23.com>
2022-05-18 10:59:44 +0100

    added vendor contact form submission method

--- src/Services/ContactService.php
+++ src/Services/ContactService.php
@@ -7,7 +7,7 @@ use Exception;
 class ContactService extends Service
 {
     /**
-     * Submit contact form to MPE
+     * Submit contact form to Tenant
      *
      * @param array $data Contact form data
      *
@@ -16,6 +16,26 @@ class ContactService extends Service
     {
         // api call
         $response = $this->http()->post($this->getPath() . '/contact-form', $data);
+
+        // call failed
+        if ($response->failed()) throw new Exception('A problem was encountered whilst attempting to submit the contact form.', 422);
+
+        // any other error
+        if ($response['error']) throw new Exception($response['message'], $response['code']);
+
+        return $response;
+    }
+
+    /**
+     * Submit contact form to Vendor
+     *
+     * @param array $data Contact form data
+     *
+     */
+    public function submitToVendor(Array $data)
+    {
+        // api call
+        $response = $this->http()->post($this->getPath() . '/contact-seller-form', $data);
 
         // call failed
         if ($response->failed()) throw new Exception('A problem was encountered whilst attempting to submit the contact form.', 422);
