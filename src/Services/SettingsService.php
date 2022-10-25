f0659e83491c87a88b9ecc2a48d5b698ab95abe8
Isaac Fennell <isaac@code23.com>
2022-10-25 18:03:46 +0100

    added settings service and method to retrieve commission groups

---
+++ src/Services/SettingsService.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Code23\MarketplaceLaravelSDK\Services;
+
+use Exception;
+
+class SettingsService extends Service
+{
+    public function getCommissionGroups($with = 'vendors')
+    {
+        // send request
+        $response = $this->http()->get($this->getPath() . '/settings/commission-groups', ['with' => $with]);
+
+        // api call failed
+        if ($response->failed()) throw new Exception('Error attempting to retrieve the commission groups.', 422);
+
+        // any other errors
+        if ($response['error']) throw new Exception($response['message'], $response['code']);
+
+        // if successful, return data as collection
+        return $response->json()['data'] ? collect($response->json()['data']) : collect();
+    }
+}
