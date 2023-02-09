dcc209d26c9119174c3918a0986ce3442de51847
Isaac Fennell <isaac@code23.com>
2023-02-09 21:33:33 +0000

    added specification service list method

--- src/Services/SpecificationService.php
+++ src/Services/SpecificationService.php
@@ -6,11 +6,10 @@ use Exception;
 
 class SpecificationService extends Service
 {
-
     /**
-     * Get a specification by id
+     * Get a specification by code
      *
-     * @param integer $id specification id to get
+     * @param string $code specification code to get
      * @param string $with optionally include comma separated relationships
      */
     public function get(String $code, String $with = 'values')
@@ -28,6 +27,36 @@ class SpecificationService extends Service
         if ($response['error']) throw new Exception($response['message'], $response['code']);
 
         // return products as collection
+        return $response->json()['data'] ? collect($response->json()['data']) : collect();
+    }
+
+    /**
+     * List specification items
+     *
+     * @param string $with optionally include comma separated relationships
+     * @param string $in optionally include comma separated specification codes to scope to, e.g.
+     */
+    public function list(String $with = 'values', String $in = null)
+    {
+        // prepare payload data
+        $payload = [
+            'with' => $with,
+        ];
+        if($in) $payload['in'] = $in;
+
+        // send request
+        $response = $this->http()->get($this->getPath() . '/specifications', $payload);
+
+        // specification not found
+        if ($response->status() == 404) throw new Exception('No specifications found', 404);
+
+        // api call failed
+        if ($response->failed()) throw new Exception('Error attempting to retrieve the specifications.', 422);
+
+        // any other errors
+        if ($response['error']) throw new Exception($response['message'], $response['code']);
+
+        // return specifications as collection
         return $response->json()['data'] ? collect($response->json()['data']) : collect();
     }
 }
