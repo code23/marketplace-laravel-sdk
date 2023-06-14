ac7e4d14f4537bacf8b4642b1c7bb23c739be36d
Pavlos Peridis <paul@paulperidis.com>
2023-06-14 17:45:18 +0300

    CAT-175

---
+++ src/Facades/MPEMessages.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Code23\MarketplaceLaravelSDK\Facades;
+
+use Code23\MarketplaceLaravelSDK\Services\MessageService;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * @method static Authenticatable get() return user
+ *
+ * @see \Code23\MarketplaceLaravelSDK\Services\UserService
+ */
+abstract class MPEMessages extends Facade
+{
+	/**
+	 * get the registered name of the component
+	 *
+	 * @return string
+	 */
+	protected static function getFacadeAccessor()
+	{
+		return MessageService::class;
+	}
+}
