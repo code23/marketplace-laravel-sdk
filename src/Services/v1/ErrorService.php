64a74e6f45ee8a72e3b7514ba319527638009ade
Isaac Fennell <isaac@code23.com>
2023-10-19 14:54:34 +0100

    WIP

--- src/Services/v1/ErrorService.php
+++ src/Services/v1/ErrorService.php
@@ -3,6 +3,7 @@
 namespace Code23\MarketplaceLaravelSDK\Services\v1;
 
 use Code23\MarketplaceLaravelSDK\Services\Service;
+use Exception;
 use Illuminate\Support\Facades\Log;
 use Spatie\SlackAlerts\Facades\SlackAlert;
 
@@ -11,8 +12,20 @@ class ErrorService extends Service
     /**
      * Alert via Slack, log, etc.
      */
-    public function report(String $filename, String $message) {
-        Log::error($filename . ': ' . $message);
-        if(env('SLACK_ALERT_WEBHOOK')) SlackAlert::message('*' . config('app.url') . "* " . $filename . ": _" . $message . "_");
+    public function handle(String $filename, String $message, Exception $e = null) {
+        // die and dump if debug is true and exception passed in
+        if (env('APP_DEBUG') && $e) return dd($e);
+
+        // generate a random error code
+        $errorCode = 'ERR-' . str_random(8);
+
+        // log the error
+        Log::error($errorCode . ' | ' . $filename . ' | ' . $message . ' | ' . $e);
+
+        // send a Slack alert if the webhook is set
+        if(env('SLACK_ALERT_WEBHOOK')) SlackAlert::message('*' . config('app.url') . "* " . $errorCode . " | " . $filename . " | _" . $message . "_");
+
+        // return the error code
+        return $errorCode;
     }
 }
