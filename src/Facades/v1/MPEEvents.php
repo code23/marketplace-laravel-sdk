a45c41ddb24dd88cbab73c19da4028cd9d408b81
Pavlos Peridis <paul@paulperidis.com>
2023-10-30 18:14:10 +0200

    CAT-381

---
+++ src/Facades/v1/MPEEvents.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Code23\MarketplaceLaravelSDK\Facades\v1;
+
+use Code23\MarketplaceLaravelSDK\Services\v1\EventService;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * @method static Authenticatable get() return user
+ *
+ * @see \Code23\MarketplaceLaravelSDK\Services\UserService
+ */
+abstract class MPEEvents extends Facade
+{
+	/**
+	 * get the registered name of the component
+	 *
+	 * @return string
+	 */
+	protected static function getFacadeAccessor()
+	{
+		return EventService::class;
+	}
+}
