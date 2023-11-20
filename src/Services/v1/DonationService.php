3788dabe971c9becaab0a3f95fbe9205c79361df
Pavlos Peridis <paul@paulperidis.com>
2023-11-20 15:18:42 +0200

    BTG-272

---
+++ src/Services/v1/DonationService.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Code23\MarketplaceLaravelSDK\Services\v1;
+
+use Code23\MarketplaceLaravelSDK\Services\Service;
+use Exception;
+
+class DonationService extends Service
+{
+	/**
+	 * Process payment
+	 */
+	public function save($charity_id, $data = [])
+	{
+		$response = $this->http()->post($this->getPath() . '/charities/' . $charity_id . '/donate', $data);
+
+		// error
+		if ($response->failed()) throw new Exception('Unable to create donation: ' . $response->body(), 422);
+
+		// any other errors
+		if (isset($response['error']) && $response['error'] == true) throw new Exception($response['message'], $response['code']);
+
+		// if successful
+		return $response->json()['data'] ? collect($response->json()['data']) : collect();
+	}
+
+	/**
+	 * Get donation by number
+	 */
+	public function getByNumber($donation_number)
+	{
+		$response = $this->http()->get($this->getPath() . '/donations/number/' . $donation_number);
+
+		// error
+		if ($response->failed()) throw new Exception('Unable to get donation: ' . $response->body(), 422);
+
+		// any other errors
+		if (isset($response['error']) && $response['error'] == true) throw new Exception($response['message'], $response['code']);
+
+		// if successful
+		return $response->json()['data'] ? collect($response->json()['data']) : collect();
+	}
+
+	/**
+	 * Get donations 
+	 */
+	public function list($params = [])
+	{
+		$response = $this->http()->get($this->getPath() . '/donations', $params);
+
+		// error
+		if ($response->failed()) throw new Exception('Unable to get donations: ' . $response->body(), 422);
+
+		// any other errors
+		if (isset($response['error']) && $response['error'] == true) throw new Exception($response['message'], $response['code']);
+
+		// if successful
+		return $response->json()['data'] ? collect($response->json()['data']) : collect();
+	}
+}
