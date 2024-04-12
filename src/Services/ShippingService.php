8f79c92777047f77a560cd6b6db07d46d8ce2fda
Pavlos Peridis <paul@paulperidis.com>
2024-04-12 20:04:27 +0300

    MPEBP-835

--- src/Services/ShippingService.php
+++ src/Services/ShippingService.php
@@ -19,10 +19,31 @@ class ShippingService extends Service
         $params = ['is_enabled' => true];
 
         // scope by vendor ids if provided
-        if(!empty($vendorIds)) $params['in'] = 'vendor_id,' . implode(',', $vendorIds);
+        if (!empty($vendorIds)) $params['in'] = 'vendor_id,' . implode(',', $vendorIds);
 
         // add to cart
         $response = $this->http()->get($this->getPath() . '/shipping/services', $params);
+
+        // api call failed
+        // if ($response->failed()) throw new Exception('Error attempting to update checkout', 422);
+
+        // any other errors
+        if ($response['error']) throw new Exception($response['message'], $response['code']);
+
+        // if successful, return cart as collection
+        return $response->json()['data'] ? collect($response->json()['data']) : collect();
+    }
+
+    /**
+     * get shipping services
+     *
+     * @param array $vendorIds - optional array of vendor ids to filter by
+     * @return void
+     */
+    public function getZones($params)
+    {
+        // add to cart
+        $response = $this->http()->get($this->getPath() . '/shipping/zones', $params);
 
         // api call failed
         // if ($response->failed()) throw new Exception('Error attempting to update checkout', 422);
