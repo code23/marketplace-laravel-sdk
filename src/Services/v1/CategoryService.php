8f4d546a064629470c95561d4bd2cb90df1cfdc0
Isaac Fennell <isaac@code23.com>
2024-07-24 13:19:27 +0100

    Adds CategoryService v1 getBySlug method

--- src/Services/v1/CategoryService.php
+++ src/Services/v1/CategoryService.php
@@ -8,6 +8,58 @@ use Exception;
 class CategoryService extends Service
 {
     /**
+     * Flatten given categories
+     */
+    public function flattenCategories($categories, $include = ['id', 'name', 'slug', 'parent_id', 'top_id'])
+    {
+        $result = [];
+
+        foreach ($categories as $category) {
+            $hold_cat = $category;
+            foreach ($hold_cat as $key => $value) {
+                // only include the keys we want
+                if(!in_array($key, $include)) unset($hold_cat[$key]);
+            }
+
+            // add the category to the result array
+            $result[] = $hold_cat;
+
+            if (isset($category['children']) && !empty($category['children'])) {
+                $children = $this->flattenCategories($category['children'], $include);
+                $result = array_merge($result, $children);
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * Retrieves a category by its slug.
+     *
+     * @param String $slug The slug of the category.
+     * @param Array $params API parameters.
+     * @throws Exception If the given category was not found, if there are errors, or if the API call fails.
+     * @return Collection The category as a collection.
+     */
+    public function getBySlug(String $slug, Array $params = [])
+    {
+        // send request
+        $response = $this->http()->get($this->getPath() . '/categories/slug/' . $slug, $params);
+
+        // category not found
+        if ($response->status() == 404) throw new Exception('The given category was not found', 404);
+
+        // errors
+        if ($response['error']) throw new Exception($response['message'], $response['code']);
+
+        // api call failed
+        if ($response->failed()) throw new Exception('Error attempting to retrieve the products by category.', 422);
+
+        // return category as collection
+        return $response->json()['data'] ? collect($response->json()['data']) : collect();
+    }
+
+    /**
      * Get a nested list of categories and subcategories
      *
      * @param array $params - See postman for available parameters
@@ -51,31 +103,5 @@ class CategoryService extends Service
 
         // if successful, return categories as collection
         return $response->json()['data'] ? collect($response->json()['data']) : collect();
-    }
-
-    /**
-     * Flatten given categories
-     */
-    public function flattenCategories($categories, $include = ['id', 'name', 'slug', 'parent_id', 'top_id'])
-    {
-        $result = [];
-
-        foreach ($categories as $category) {
-            $hold_cat = $category;
-            foreach ($hold_cat as $key => $value) {
-                // only include the keys we want
-                if(!in_array($key, $include)) unset($hold_cat[$key]);
-            }
-
-            // add the category to the result array
-            $result[] = $hold_cat;
-
-            if (isset($category['children']) && !empty($category['children'])) {
-                $children = $this->flattenCategories($category['children'], $include);
-                $result = array_merge($result, $children);
-            }
-        }
-
-        return $result;
     }
 }
