194787a4bdd92b79571e32dc0b7d761021207cbc
habib <chaudryhabib2@gmail.com>
2024-10-18 14:47:48 +0500

    MARKET-4280: method to get vendor reviews

--- src/Services/v1/BookingReviewService.php
+++ src/Services/v1/BookingReviewService.php
@@ -122,4 +122,51 @@ class BookingReviewService extends Service
         if (isset($params['paginate']) && $params['paginate']) return $response->json() ? collect($response->json()) : collect();
         return $response->json()['data'] ? collect($response->json()['data']) : collect();
     }
+
+    /**
+     * Return a list of vendor reviews.
+     *
+     * By default returns all reviews.
+     *
+     * @param Int $vendor_id
+     *
+     * @param Int $paginate
+     *      Pagination items per page. Defaults to all.
+     *
+     * @param Int $page
+     *      Pagination page to retrieve. Defaults to 1.
+     *
+     * @param String $with
+     *      Relationships to include
+     *
+     * @param String $sort
+     *      sort results by
+
+     * @return Collection
+     */
+    public function listVendorReviews(int $vendor_id, $paginate = 0, $page = 1, $with = '', $sort = null)
+    {
+        // create params & include relationships
+        $params = ['with' => $with];
+
+        // paginate results
+        if($paginate) $params['paginate'] = $paginate;
+        if($page > 1) $params['page'] = $page;
+
+        // sort results
+        if($sort) $params['sort'] = $sort;
+
+        // call to api
+        $response = $this->http()->get($this->getPath() . '/booking-reviews/vendors/' . $vendor_id, $params);
+
+        // api call failed
+        if ($response->failed()) throw new Exception('Unable to retrieve the reviews!', 422);
+
+        // any other error
+        if ($response['error']) throw new Exception($response['message'], $response['code']);
+
+        // return reviews list
+        if (isset($params['paginate']) && $params['paginate']) return $response->json() ? collect($response->json()) : collect();
+        return $response->json()['data'] ? collect($response->json()['data']) : collect();
+    }
 }
